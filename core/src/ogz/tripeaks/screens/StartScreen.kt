111562b5f070e5c6e8c3dba4b9841bc975f7081c
mimoguz <mimoguz@users.noreply.github.com>
2021-03-24 19:20:48 +0300

    Remove screen cleaning

--- core/src/ogz/tripeaks/screens/StartScreen.kt
+++ core/src/ogz/tripeaks/screens/StartScreen.kt
@@ -1,6 +1,7 @@
 package ogz.tripeaks.screens
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.Stage
@@ -16,52 +17,54 @@ import ogz.tripeaks.data.GamePreferences
 class StartScreen(val game: Game) : KtxScreen {
 
     private val stage = Stage(
-            IntegerScalingViewport(
-                    Const.CONTENT_WIDTH.toInt(),
-                    Const.CONTENT_HEIGHT.toInt(),
-                    OrthographicCamera()
-            )
+        IntegerScalingViewport(
+            Const.CONTENT_WIDTH.toInt(),
+            Const.CONTENT_HEIGHT.toInt(),
+            OrthographicCamera()
+        )
     )
 
-    private var clearColor = Const.LIGHT_BACKGROUND
+    override fun dispose() {
+        stage.dispose()
+    }
 
     override fun render(delta: Float) {
         stage.act(delta)
-        ScreenUtils.clear(clearColor)
         stage.draw()
     }
 
     override fun show() {
         val preferences = GamePreferences().load()
         val skin = Scene2DSkin.defaultSkin
-        if (preferences.useDarkTheme) {
-            clearColor = Const.DARK_BACKGROUND
-        }
-        val image = if (preferences.useDarkTheme) game.assets[TextureAssets.DarkTitle] else game.assets[TextureAssets.LightTitle]
+        val image =
+            if (preferences.useDarkTheme) game.assets[TextureAssets.DarkTitle] else game.assets[TextureAssets.LightTitle]
         stage.actors.add(
-                Image(image).apply {
-                    setSize(300f, 168f)
-                    setPosition(0f, 0f)
-                },
+            Image(image).apply {
+                setSize(300f, 168f)
+                setPosition(0f, 0f)
+            },
 
-                TextButton(
-                        game.assets[BundleAssets.Bundle].get("start"),
-                        skin,
-                        preferences.themeKey
-                ).apply {
-                    pad(6f, 8f, 5f, 8f)
-                    addListener(object : ChangeListener() {
-                        override fun changed(event: ChangeEvent?, actor: Actor?) {
-                            game.addScreen(GameScreen(game, preferences))
-                            game.setScreen<GameScreen>()
-                            game.removeScreen<StartScreen>()
-                            dispose()
-                        }
+            TextButton(
+                game.assets[BundleAssets.Bundle].get("start"),
+                skin,
+                preferences.themeKey
+            ).apply {
+                pad(6f, 8f, 5f, 8f)
+                addListener(object : ChangeListener() {
+                    override fun changed(event: ChangeEvent?, actor: Actor?) {
+                        game.addScreen(GameScreen(game, preferences))
+                        game.setScreen<GameScreen>()
+                        game.removeScreen<StartScreen>()
+                        dispose()
+                    }
 
-                    })
-                    width = 100f
-                    setPosition((Const.CONTENT_WIDTH - width) / 2f, (Const.CONTENT_HEIGHT - height) / 2f)
-                }
+                })
+                width = 100f
+                setPosition(
+                    (Const.CONTENT_WIDTH - width) / 2f,
+                    (Const.CONTENT_HEIGHT - height) / 2f
+                )
+            }
         )
         Gdx.input.inputProcessor = stage
     }
