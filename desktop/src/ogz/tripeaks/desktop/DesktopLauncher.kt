828ebfe605e00b6b025195c1fe72cb36fc04768c
mimoguz <mimoguz@users.noreply.github.com>
2021-08-08 00:37:40 +0300

    Set initial window size

--- desktop/src/ogz/tripeaks/desktop/DesktopLauncher.kt
+++ desktop/src/ogz/tripeaks/desktop/DesktopLauncher.kt
@@ -1,20 +1,24 @@
 package ogz.tripeaks.desktop
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import ogz.tripeaks.Const
 import ogz.tripeaks.Game
 
 object DesktopLauncher {
     @JvmStatic
     fun main(arg: Array<String>) {
-        val config = Lwjgl3ApplicationConfiguration()
-        config.setTitle("TriPeaks")
-        config.setWindowIcon(
-            "images/icon16.png",
-            "images/icon24.png",
-            "images/icon32.png",
-            "images/icon48.png",
-        )
+        val config = Lwjgl3ApplicationConfiguration().apply {
+            setTitle("TriPeaks")
+            setWindowIcon(
+                "images/icon16.png",
+                "images/icon24.png",
+                "images/icon32.png",
+                "images/icon48.png",
+            )
+            setWindowedMode(Const.CONTENT_WIDTH.toInt() * 3, Const.CONTENT_HEIGHT.toInt() * 3)
+        }
         Lwjgl3Application(Game(), config)
     }
 }
