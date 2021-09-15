e42cb7e42b8b9bb69b2027689e0f2a62b54369ff
mimoguz <mimoguz@users.noreply.github.com>
2021-09-15 15:53:08 +0300

    Statistics dialog draft 2

--- core/src/ogz/tripeaks/screens/dialogs/StatisticsDialog.kt
+++ core/src/ogz/tripeaks/screens/dialogs/StatisticsDialog.kt
@@ -1,7 +1,7 @@
 package ogz.tripeaks.screens.dialogs
 
-import com.badlogic.gdx.scenes.scene2d.ui.Dialog
-import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.*
+import com.badlogic.gdx.utils.Align
 import com.badlogic.gdx.utils.I18NBundle
 import ogz.tripeaks.Const
 import ogz.tripeaks.game.Statistics
@@ -16,35 +16,34 @@ class StatisticsDialog(
     val res: I18NBundle,
 ) : Dialog("", skinData.skin, theme) {
     init {
-        pad(16f, 24f, 16f, 24f)
-        contentTable.apply {
-            add(
-                Label(
-                    "${statistics.gamesPlayed} ${res.format("statGame", statistics.gamesPlayed)}",
-                    skinData.skin
-                )
-            )
-            add(
-                Label(
-                    "${statistics.gamesWon} ${res.format("statWin", statistics.gamesWon)}",
-                    skinData.skin
-                )
-            )
-            row()
+        pad(12f, 8f, 12f, 8f)
+        val table = Table(skinData.skin).apply {
+            defaults().align(Align.left).pad(0f).space(0f, 0f, 0f, 12f)
 
-            add(Label("", skinData.skin))
-            add(Label(res.get("statGames"), skinData.skin))
-            add(Label(res.get("statWins"), skinData.skin))
-            add(Label(res.get("statLongestChain"), skinData.skin))
+            add(Label(res.get("statAll"), skinData.skin)).colspan(3)
+            row()
+            add(Label("${res.get("statGames")}: ${statistics.gamesPlayed}", skinData.skin))
+            add(Label("${res.get("statWins")}: ${statistics.gamesWon}", skinData.skin))
+            // TODO: Longest chain
+            add(Label("${res.get("statLongestChain")}: TODO", skinData.skin))
             row()
 
             for (layout in statistics.perLayoutStatistics.values()) {
-                add(Label("${layout.tag}", skinData.skin))
-                add(Label("${layout.played}", skinData.skin))
-                add(Label("${layout.won}", skinData.skin))
-                add(Label("${layout.longestChain}", skinData.skin))
+                add(Label("${res.get(layout.tag)}", skinData.skin)).colspan(3).padTop(8f)
+                row()
+                add(Label("${res.get("statGames")}: ${layout.played}", skinData.skin))
+                add(Label("${res.get("statWins")}: ${layout.won}", skinData.skin))
+                add(Label("${res.get("statLongestChain")}: ${layout.longestChain}", skinData.skin))
                 row()
             }
+        }
+        val scroll = ScrollPane(table, skinData.skin).apply {
+            setScrollbarsVisible(true)
+            fadeScrollBars = false
+        }
+        contentTable.apply {
+            pad(0f)
+            add(scroll).pad(0f).height(90f).width(280f)
         }
         buttonTable.apply {
             pad(4f, 4f, 0f, 4f)
