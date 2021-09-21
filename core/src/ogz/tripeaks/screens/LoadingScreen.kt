12b177cd79e61c1582d749725b2c631597a2523e
mimoguz <mimoguz@users.noreply.github.com>
2021-09-21 23:59:35 +0300

    Scroll and title styles

--- core/src/ogz/tripeaks/screens/LoadingScreen.kt
+++ core/src/ogz/tripeaks/screens/LoadingScreen.kt
@@ -15,14 +15,14 @@ import ogz.tripeaks.util.GamePreferences
 import ogz.tripeaks.util.SkinData
 
 class LoadingScreen(
-    private val game: Game,
-    private val batch: Batch,
-    private val viewport: Viewport,
-    private val assets: AssetManager,
-    private val preferences: GamePreferences,
-    private val layouts: List<Layout>
+        private val game: Game,
+        private val batch: Batch,
+        private val viewport: Viewport,
+        private val assets: AssetManager,
+        private val preferences: GamePreferences,
+        private val layouts: List<Layout>
 ) :
-    KtxScreen {
+        KtxScreen {
 
     override fun show() {
         for (asset in TextureAtlasAssets.values()) assets.load(asset)
@@ -59,143 +59,160 @@ class LoadingScreen(
     }
 
     private fun makeCommonSkin(skinFont: BitmapFont): Skin =
-        skin(assets[TextureAtlasAssets.Ui]) { skin ->
-            color("light", 242f / 255f, 204f / 255f, 143f / 255f, 1f)
-            color("dark", 76f / 255f, 56f / 255f, 77f / 255f, 1f)
-            label {
-                font = skinFont
-                fontColor = skin["dark"]
+            skin(assets[TextureAtlasAssets.Ui]) { skin ->
+
+                color("light", 242f / 255f, 204f / 255f, 143f / 255f, 1f)
+                color("dark", 76f / 255f, 56f / 255f, 77f / 255f, 1f)
+                color("emphasisLight", 184f / 255f, 55f / 255f, 68f / 255f, 1f)
+                color("emphasisDark", 224f / 255f, 122f / 255f, 95f / 255f, 1f)
+
+                label {
+                    font = skinFont
+                    fontColor = skin["dark"]
+                }
+                label("light", extend = defaultStyle)
+                label("dark", extend = defaultStyle) {
+                    fontColor = skin["light"]
+                }
+                label("title_light", extend = defaultStyle) {
+                    fontColor = skin["emphasisLight"]
+                }
+                label("title_dark", extend = defaultStyle) {
+                    fontColor = skin["emphasisDark"]
+                }
+
+                button {
+                    up = skin["buttonUp"]
+                    down = skin["buttonDown"]
+                    disabled = skin["buttonDisabled"]
+                    pressedOffsetY = -1f
+                }
+                button("light", extend = defaultStyle)
+                button("dark", extend = defaultStyle) {
+                    up = skin["buttonUp_dark"]
+                    down = skin["buttonDown_dark"]
+                    disabled = skin["buttonDisabled_dark"]
+                }
+
+                textButton {
+                    up = skin["buttonUp"]
+                    down = skin["buttonDown"]
+                    disabled = skin["buttonDisabled"]
+                    font = skinFont
+                    fontColor = skin["dark"]
+                    pressedOffsetY = -1f
+                }
+                textButton("light", extend = defaultStyle)
+                textButton("dark", extend = defaultStyle) {
+                    up = skin["buttonUp_dark"]
+                    down = skin["buttonDown_dark"]
+                    disabled = skin["buttonDisabled_dark"]
+                    fontColor = skin["light"]
+                }
+                textButton("menuItem_light") {
+                    up = skin["menuItemUp"]
+                    down = skin["menuItemDown"]
+                    disabled = skin["menuItemUp"]
+                    over = skin["menuItemOver"]
+                    font = skinFont
+                    fontColor = skin["dark"]
+                }
+                textButton("menuItem_dark") {
+                    up = skin["menuItemUp_dark"]
+                    down = skin["menuItemDown_dark"]
+                    disabled = skin["menuItemUp_dark"]
+                    over = skin["menuItemOver_dark"]
+                    font = skinFont
+                    fontColor = skin["light"]
+                }
+                textButton("redButton_light") {
+                    up = skin["redButtonUp"]
+                    down = skin["redButtonDown"]
+                    disabled = skin["redButtonDisabled"]
+                    font = skinFont
+                    fontColor = skin["light"]
+                    pressedOffsetY = -1f
+                }
+                textButton("redButton_dark", extend = "redButton_light") {
+                    up = skin["redButtonUp_dark"]
+                    down = skin["redButtonDown_dark"]
+                    disabled = skin["redButtonDisabled_dark"]
+                }
+
+                window {
+                    titleFont = skinFont
+                    titleFontColor = skin["dark"]
+                    background = skin["window"]
+                }
+                window("light", extend = defaultStyle)
+                window("dark", extend = defaultStyle) {
+                    titleFontColor = skin["light"]
+                    background = skin["window_dark"]
+                }
+                checkBox {
+                    checkboxOn = skin["checkboxOn"]
+                    checkboxOff = skin["checkboxOff"]
+                    font = skinFont
+                    fontColor = skin["dark"]
+                }
+
+                checkBox("light", extend = defaultStyle)
+                checkBox("dark", extend = defaultStyle) {
+                    checkboxOn = skin["checkboxOn_dark"]
+                    checkboxOff = skin["checkboxOff_dark"]
+                    fontColor = skin["light"]
+                }
+
+                val listStyleDefault =  list {
+                    font = skinFont
+                    fontColorSelected = skin["dark"]
+                    fontColorUnselected = skin["dark"]
+                    selection = skin["menuItemDown"]
+                    over = skin["menuItemOver"]
+                    background = skin["window"]
+                }
+                list("light", extend = defaultStyle)
+                val listStyleDark = list("dark", extend = defaultStyle) {
+                    fontColorSelected = skin["light"]
+                    fontColorUnselected = skin["light"]
+                    selection = skin["menuItemDown_dark"]
+                    over = skin["menuItemOver_dark"]
+                    background = skin["window_dark"]
+                }
+
+                val scrollPaneStyleDefault =  scrollPane {
+                    hScroll = skin["scroll"]
+                    hScrollKnob = skin["scrollKnob"]
+                    vScroll = skin["scroll"]
+                    vScrollKnob = skin["scrollKnob"]
+                    corner = skin["menuItemUp"]
+                }
+                scrollPane("light", extend = defaultStyle)
+                val scrollPaneStyleDark = scrollPane("dark", extend = defaultStyle) {
+                    hScroll = skin["scroll_dark"]
+                    hScrollKnob = skin["scrollKnob_dark"]
+                    vScroll = skin["scroll_dark"]
+                    vScrollKnob = skin["scrollKnob_dark"]
+                    corner = skin["menuItemUp_dark"]
+                }
+
+                selectBox {
+                    font = skinFont
+                    fontColor = skin["dark"]
+                    background = skin["selectBoxUp"]
+                    backgroundOpen = skin["selectBoxDown"]
+                    scrollStyle = scrollPaneStyleDefault
+                    listStyle = listStyleDefault
+                }
+                selectBox("light", extend = defaultStyle)
+                selectBox("dark", extend = defaultStyle) {
+                    fontColor = skin["light"]
+                    background = skin["selectBoxUp_dark"]
+                    backgroundOpen = skin["selectBoxDown_dark"]
+                    scrollStyle = scrollPaneStyleDark
+                    listStyle = listStyleDark
+                }
             }
-            label("light", extend = defaultStyle)
-            label("dark", extend = defaultStyle) {
-                fontColor = skin["light"]
-            }
-            button {
-                up = skin["buttonUp"]
-                down = skin["buttonDown"]
-                disabled = skin["buttonDisabled"]
-                pressedOffsetY = -1f
-            }
-            button("light", extend = defaultStyle)
-            button("dark", extend = defaultStyle) {
-                up = skin["buttonUp_dark"]
-                down = skin["buttonDown_dark"]
-                disabled = skin["buttonDisabled_dark"]
-            }
-            textButton {
-                up = skin["buttonUp"]
-                down = skin["buttonDown"]
-                disabled = skin["buttonDisabled"]
-                font = skinFont
-                fontColor = skin["dark"]
-                pressedOffsetY = -1f
-            }
-            textButton("light", extend = defaultStyle)
-            textButton("dark", extend = defaultStyle) {
-                up = skin["buttonUp_dark"]
-                down = skin["buttonDown_dark"]
-                disabled = skin["buttonDisabled_dark"]
-                fontColor = skin["light"]
-            }
-            textButton("menuItem_light") {
-                up = skin["menuItemUp"]
-                down = skin["menuItemDown"]
-                disabled = skin["menuItemUp"]
-                over = skin["menuItemOver"]
-                font = skinFont
-                fontColor = skin["dark"]
-            }
-            textButton("menuItem_dark") {
-                up = skin["menuItemUp_dark"]
-                down = skin["menuItemDown_dark"]
-                disabled = skin["menuItemUp_dark"]
-                over = skin["menuItemOver_dark"]
-                font = skinFont
-                fontColor = skin["light"]
-            }
-            textButton("redButton_light") {
-                up = skin["redButtonUp"]
-                down = skin["redButtonDown"]
-                disabled = skin["redButtonDisabled"]
-                font = skinFont
-                fontColor = skin["light"]
-                pressedOffsetY = -1f
-            }
-            textButton("redButton_dark", extend = "redButton_light") {
-                up = skin["redButtonUp_dark"]
-                down = skin["redButtonDown_dark"]
-                disabled = skin["redButtonDisabled_dark"]
-            }
-            window {
-                titleFont = skinFont
-                titleFontColor = skin["dark"]
-                background = skin["window"]
-            }
-            window("light", extend = defaultStyle)
-            window("dark", extend = defaultStyle) {
-                titleFontColor = skin["light"]
-                background = skin["window_dark"]
-            }
-            checkBox {
-                checkboxOn = skin["checkboxOn"]
-                checkboxOff = skin["checkboxOff"]
-                font = skinFont
-                fontColor = skin["dark"]
-            }
-            checkBox("light", extend = defaultStyle)
-            checkBox("dark", extend = defaultStyle) {
-                checkboxOn = skin["checkboxOn_dark"]
-                checkboxOff = skin["checkboxOff_dark"]
-                fontColor = skin["light"]
-            }
-            val listStyleDefault =  list {
-                font = skinFont
-                fontColorSelected = skin["dark"]
-                fontColorUnselected = skin["dark"]
-                selection = skin["menuItemDown"]
-                over = skin["menuItemOver"]
-                background = skin["window"]
-            }
-            list("light", extend = defaultStyle)
-            val listStyleDark = list("dark", extend = defaultStyle) {
-                fontColorSelected = skin["light"]
-                fontColorUnselected = skin["light"]
-                selection = skin["menuItemDown_dark"]
-                over = skin["menuItemOver_dark"]
-                background = skin["window_dark"]
-            }
-            val scrollPaneStyleDefault =  scrollPane {
-                hScroll = skin["menuItemDown"]
-                hScrollKnob = skin["buttonUp"]
-                vScroll = skin["menuItemDown"]
-                vScrollKnob = skin["buttonUp"]
-                corner = skin["menuItemUp"]
-            }
-            scrollPane("light", extend = defaultStyle)
-            val scrollPaneStyleDark = scrollPane("dark", extend = defaultStyle) {
-                hScroll = skin["menuItemDown_dark"]
-                hScrollKnob = skin["buttonUp_dark"]
-                vScroll = skin["menuItemDown_dark"]
-                vScrollKnob = skin["buttonUp_dark"]
-                corner = skin["menuItemUp_dark"]
-            }
-            selectBox {
-                font = skinFont
-                fontColor = skin["dark"]
-                background = skin["selectBoxUp"]
-                backgroundOpen = skin["selectBoxDown"]
-                scrollStyle = scrollPaneStyleDefault
-                listStyle = listStyleDefault
-            }
-            selectBox("light", extend = defaultStyle)
-            selectBox("dark", extend = defaultStyle) {
-                fontColor = skin["light"]
-                background = skin["selectBoxUp_dark"]
-                backgroundOpen = skin["selectBoxDown_dark"]
-                scrollStyle = scrollPaneStyleDark
-                listStyle = listStyleDark
-            }
-        }
 
     override fun resize(width: Int, height: Int) {
         viewport.update(width, height)
--- core/src/ogz/tripeaks/screens/dialogs/StatisticsDialog.kt
+++ core/src/ogz/tripeaks/screens/dialogs/StatisticsDialog.kt
@@ -16,34 +16,38 @@ class StatisticsDialog(
     val res: I18NBundle,
 ) : Dialog("", skinData.skin, theme) {
     init {
+        val titleStyle = "title_$theme"
+        val tableWidth = 250f
         pad(12f, 8f, 12f, 8f)
+        width = tableWidth + padLeft + padRight
         val table = Table(skinData.skin).apply {
             defaults().align(Align.left).pad(0f).space(0f, 0f, 0f, 12f)
+            width = tableWidth
 
-            add(Label(res.get("statAll"), skinData.skin)).colspan(3)
+            add(Label(res.get("statAll"), skinData.skin, titleStyle)).colspan(3)
             row()
-            add(Label("${res.get("statGames")}: ${statistics.gamesPlayed}", skinData.skin))
-            add(Label("${res.get("statWins")}: ${statistics.gamesWon}", skinData.skin))
+            add(Label("${res.get("statGames")}: ${statistics.gamesPlayed}", skinData.skin, theme))
+            add(Label("${res.get("statWins")}: ${statistics.gamesWon}", skinData.skin, theme))
             // TODO: Longest chain
-            add(Label("${res.get("statLongestChain")}: TODO", skinData.skin))
+            add(Label("${res.get("statLongestChain")}: TODO", skinData.skin, theme)).spaceRight(0f)
             row()
 
             for (layout in statistics.perLayoutStatistics.values()) {
-                add(Label("${res.get(layout.tag)}", skinData.skin)).colspan(3).padTop(8f)
+                add(Label("${res.get(layout.tag)}", skinData.skin, titleStyle)).colspan(3).padTop(8f)
                 row()
-                add(Label("${res.get("statGames")}: ${layout.played}", skinData.skin))
-                add(Label("${res.get("statWins")}: ${layout.won}", skinData.skin))
-                add(Label("${res.get("statLongestChain")}: ${layout.longestChain}", skinData.skin))
+                add(Label("${res.get("statGames")}: ${layout.played}", skinData.skin, theme))
+                add(Label("${res.get("statWins")}: ${layout.won}", skinData.skin, theme))
+                add(Label("${res.get("statLongestChain")}: ${layout.longestChain}", skinData.skin, theme)).spaceRight(0f)
                 row()
             }
         }
-        val scroll = ScrollPane(table, skinData.skin).apply {
+        val scroll = ScrollPane(table, skinData.skin, theme).apply {
             setScrollbarsVisible(true)
             fadeScrollBars = false
         }
         contentTable.apply {
             pad(0f)
-            add(scroll).pad(0f).height(90f).width(280f)
+            add(scroll).pad(0f).height(100f).width(tableWidth)
         }
         buttonTable.apply {
             pad(4f, 4f, 0f, 4f)
