af1879e760d88e727285748269ebeb3bd1fb0b45
mimoguz <mimoguz@users.noreply.github.com>
2021-10-31 12:53:05 +0300

    Statistic dialog & Italian translation

--- core/src/ogz/tripeaks/screens/dialogs/StatisticsDialog.kt
+++ core/src/ogz/tripeaks/screens/dialogs/StatisticsDialog.kt
@@ -20,14 +20,15 @@ class StatisticsDialog(
     init {
         val titleStyle = "title_$theme"
 
-        pad(4f, 5f, 12f, 5f)
-
+        pad(4f, 16f, 12f, 16f)
 
         val table = Table(skinData.skin).apply {
             // debug()
-            align(Align.left)
-            defaults().align(Align.left).pad(0f).space(0f, 0f, 0f, 8f)
-            width = 260f
+            val vspace = if (res.get("skinKey") == "cjk") 8f else 0f
+            defaults().align(Align.left).pad(0f).space(vspace, 0f, vspace, 12f)
+            width = 220f
+            align(Align.center)
+            padLeft(16f)
 
             val games = statistics.perLayoutStatistics.values().sumOf { it.played }
             val wins = statistics.perLayoutStatistics.values().sumOf { it.won }
@@ -35,36 +36,37 @@ class StatisticsDialog(
                 if (acc > layout.longestChain) acc else layout.longestChain
             }
 
-            add(Label(res.get("statAll"), skinData.skin, titleStyle)).colspan(3)
+            add(Label(res.get("statAll"), skinData.skin, titleStyle)).colspan(2).align(Align.center)
             row()
-            add(Label("${res.get("statGames")}: $games", skinData.skin, theme))
-            add(Label("${res.get("statWins")}: $wins", skinData.skin, theme))
-            add(
-                Label(
-                    "${res.get("statLongestChain")}: $longestChain",
-                    skinData.skin,
-                    theme
-                )
-            ).spaceRight(0f)
+            add(Label(res.get("statGames"), skinData.skin, theme))
+            add(Label(games.toString(), skinData.skin, theme)).align(Align.right)
             row()
+            add(Label(res.get("statWins"), skinData.skin, theme))
+            add(Label(wins.toString(), skinData.skin, theme)).align(Align.right)
+            row()
+            add(Label(res.get("statLongestChain"), skinData.skin, theme))
+            add(Label(longestChain.toString(), skinData.skin, theme)).align(Align.right)
 
             for (layout in statistics.perLayoutStatistics.values().filter { it.played > 0 }
                 .sortedByDescending { it.played }) {
-                add(Image(ui.findRegion("line_$theme"))).width(this.width / 2f - 6f).colspan(3)
-                    .align(Align.left).padTop(5f).padBottom(-2f)
                 row()
-                add(Label(res.get(layout.tag), skinData.skin, titleStyle)).colspan(3).padTop(8f)
+                //TODO: Find a better way to handle the bottom padding
+                add(Image(ui.findRegion("line_$theme"))).width(32f)
+                    .colspan(2)
+                    .align(Align.center)
+                    .pad(6f, 0f, 6f, 0f)
                 row()
-                add(Label("${res.get("statGames")}: ${layout.played}", skinData.skin, theme))
-                add(Label("${res.get("statWins")}: ${layout.won}", skinData.skin, theme))
-                add(
-                    Label(
-                        "${res.get("statLongestChain")}: ${layout.longestChain}",
-                        skinData.skin,
-                        theme
-                    )
-                ).spaceRight(0f)
+                add(Label(res.get(layout.tag), skinData.skin, titleStyle)).colspan(2)
+                    .align(Align.center)
                 row()
+                add(Label(res.get("statGames"), skinData.skin, theme))
+                add(Label(layout.played.toString(), skinData.skin, theme)).align(Align.right)
+                row()
+                add(Label(res.get("statWins"), skinData.skin, theme))
+                add(Label(layout.won.toString(), skinData.skin, theme)).align(Align.right)
+                row()
+                add(Label(res.get("statLongestChain"), skinData.skin, theme))
+                add(Label(layout.longestChain.toString(), skinData.skin, theme)).align(Align.right)
             }
         }
 
@@ -79,13 +81,13 @@ class StatisticsDialog(
             pad(0f)
             add(Image(ui.findRegion("stats_$theme"))).pad(0f).space(0f)
             row()
-            add(scroll).pad(0f).padLeft(11f).height(96f).width(table.width)
+            add(scroll).pad(0f).height(96f).width(table.width)
         }
 
         buttonTable.apply {
             // More padding on the left to compensate uneven window padding:
             pad(4f, 4f, 0f, 4f)
-            defaults().width(108f).space(4f).height(Const.BUTTON_HEIGHT).pad(0f)
+            defaults().width(158f).space(4f).height(Const.BUTTON_HEIGHT).pad(0f)
             add(MyTextButton(res.get("return"), skinData, theme).apply {
                 setAction { hide() }
             })
