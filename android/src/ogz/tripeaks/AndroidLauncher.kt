db4a6886c2905c739af30089e487b88619b81bf1
mimoguz <mimoguz@users.noreply.github.com>
2022-06-18 11:47:41 +0300

    Use immersive mode

--- android/src/ogz/tripeaks/AndroidLauncher.kt
+++ android/src/ogz/tripeaks/AndroidLauncher.kt
@@ -8,6 +8,7 @@ class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val config = AndroidApplicationConfiguration()
+        config.useImmersiveMode = true;
         initialize(Game(), config)
     }
 }
