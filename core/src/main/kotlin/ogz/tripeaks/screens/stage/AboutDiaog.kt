ada5ff546c5e1f2a71700cc6ccae0b5b21113a3d
Oguz Tas <mimoguz@outlook.com>
2024-08-13 00:32:08 +0300

    Section panels

--- core/src/main/kotlin/ogz/tripeaks/screens/stage/AboutDiaog.kt
+++ core/src/main/kotlin/ogz/tripeaks/screens/stage/AboutDiaog.kt
@@ -1,12 +1,15 @@
 package ogz.tripeaks.screens.stage
 
 import com.badlogic.gdx.assets.AssetManager
+import com.badlogic.gdx.graphics.g2d.TextureAtlas
 import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.scenes.scene2d.ui.Cell
 import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup
 import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane
 import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable
 import com.badlogic.gdx.utils.Align
 import com.ray3k.stripe.PopTable
 import ogz.tripeaks.Constants
@@ -16,10 +19,7 @@ import ogz.tripeaks.assets.UiSkin
 import ogz.tripeaks.assets.get
 import ogz.tripeaks.ui.LabelButton
 
-class AboutDiaog(
-    skin: UiSkin,
-    assets: AssetManager,
-) : PopTable(skin) {
+class AboutDiaog(skin: UiSkin, assets: AssetManager) : PopTable(skin) {
 
     init {
         val bundle = assets[BundleAssets.Bundle]
@@ -48,45 +48,49 @@ class AboutDiaog(
         row()
 
         val table = Table(skin).apply {
-            defaults()
-                .align(Align.left)
-                .padBottom(skin.extraLineSpacing)
-                .padRight(Constants.UI_HORIZONTAL_SPACING)
-                .expandX()
-                .fillX()
+            padRight(Constants.UI_HORIZONTAL_SPACING)
+            defaults().align(Align.left).expandX().fillX()
 
-            add(Label("Licenses", skin, UiSkin.TITLE_LABEL_STYLE_LATIN))
-            row()
-            add(Image(line)).pad(divSpacing, 0f, divSpacing, Constants.UI_HORIZONTAL_SPACING)
-            row()
-            addLicenseInfo(this, "tripeaks-gdx", "GPL-3.0", "github.com/mimoguz/tripeaks-gdx")
-            addLicenseInfo(this, "libGDX", "Apache-2.0", "libgdx.com")
-            addLicenseInfo(this, "KTX", "CC0-1.0", "libktx.github.io")
-            addLicenseInfo(this, "Stripe", "MIT", "github.com/raeleus/stripe")
-            row()
-            addLicenseInfo(
-                this,
-                "GNU Unifont",
-                "OFL-1.1, or GPL-2+ with\nthe GNU font embedding exception",
-                "unifoundry.com/unifont"
-            )
-
-            add(Label("Privacy Policy", skin, UiSkin.TITLE_LABEL_STYLE_LATIN))
-            row()
-            add(Image(line)).pad(divSpacing, 0f, divSpacing, Constants.UI_HORIZONTAL_SPACING)
-            row()
             add(
-                Label(
-                    "This game is open source and free\n" +
-                            "software. It is compiled unchanged\n" +
-                            "from the source codes available at\n" +
-                            "github.com/mimoguz/tripeaks-gdx.\n" +
-                            "It does not collect any user data.",
-                    skin,
-                    UiSkin.LATIN_LABEL_STYLE
-                )
+                sectionPanel(uiAssets, "Licenses").apply {
+                    addLicenseInfo("tripeaks-gdx", "GPL-3.0", "github.com/mimoguz/tripeaks-gdx")
+                        .padBottom(Constants.UI_VERTICAL_SPACING + 2f)
+                    row()
+                    addLicenseInfo("libGDX", "Apache-2.0", "libgdx.com")
+                        .padBottom(Constants.UI_VERTICAL_SPACING + 2f)
+                    row()
+                    addLicenseInfo("KTX", "CC0-1.0", "libktx.github.io")
+                        .padBottom(Constants.UI_VERTICAL_SPACING + 2f)
+                    row()
+                    addLicenseInfo("Stripe", "MIT", "github.com/raeleus/stripe")
+                        .padBottom(Constants.UI_VERTICAL_SPACING + 2f)
+                    row()
+                    addLicenseInfo(
+                        "GNU Unifont",
+                        "OFL-1.1, or GPL-2+ with\nthe GNU font embedding exception",
+                        "unifoundry.com/unifont"
+                    )
+                }
             )
-                .padLeft(Constants.UI_HORIZONTAL_SPACING * 2f)
+                .padBottom(Constants.UI_VERTICAL_SPACING)
+
+            row()
+
+            add(
+                sectionPanel(uiAssets, "Privacy Policy").apply {
+                    add(
+                        Label(
+                            "This game is open source and free\n" +
+                                    "software. It is compiled unchanged\n" +
+                                    "from the source codes available at\n" +
+                                    "github.com/mimoguz/tripeaks-gdx.\n" +
+                                    "It does not collect any user data.",
+                            skin,
+                            UiSkin.LATIN_LABEL_STYLE
+                        )
+                    )
+                }
+            )
         }
 
         val scroll = ScrollPane(table, skin).apply {
@@ -112,24 +116,48 @@ class AboutDiaog(
         isHideOnUnfocus = false
     }
 
-    private fun addLicenseInfo(
-        table: Table,
+    private fun Table.addLicenseInfo(
         projectName: String,
         license: String,
         link: String,
-    ) {
-        val indent = Constants.UI_HORIZONTAL_SPACING * 2f
-        val uiSkin = skin as UiSkin
-
-        table.apply {
-            add(Label("$projectName: $license.", skin, UiSkin.LATIN_LABEL_STYLE)).padLeft(indent)
-                .padBottom(2f)
+    ): Cell<Label> = this.let {
+            add(Label("$projectName: $license.", skin, UiSkin.LATIN_LABEL_STYLE)).padBottom(2f)
             row()
             add(Label(link, skin, UiSkin.LATIN_LABEL_STYLE))
-                .padBottom(Constants.UI_HORIZONTAL_SPACING + 2f)
-                .colspan(2)
-                .padLeft(indent)
-            row()
         }
+
+    private fun sectionPanel(uiAssets: TextureAtlas, header: String): Table = Table(skin).apply {
+        val uiSkin = skin as UiSkin
+        val line = uiAssets.findRegion("${uiSkin.resourcePrefix}_line")
+        val divSpacing = MathUtils.floor(uiSkin.extraLineSpacing / 2f).toFloat().coerceAtLeast(2f)
+        val bg = NinePatchDrawable(uiAssets.createPatch("${uiSkin.resourcePrefix}_panel"))
+
+        pad(
+            Constants.UI_VERTICAL_SPACING,
+            Constants.UI_HORIZONTAL_SPACING,
+            Constants.UI_VERTICAL_SPACING,
+            Constants.UI_HORIZONTAL_SPACING,
+        )
+        setBackground(bg)
+        defaults().padBottom(uiSkin.extraLineSpacing.coerceAtLeast(2f)).left()
+
+        add(Label(header, skin, UiSkin.TITLE_LABEL_STYLE))
+            .colspan(2)
+            .align(Align.left)
+
+        row()
+
+        add(Image(line))
+            .colspan(2)
+            .expandX()
+            .fillX()
+            .pad(
+                divSpacing,
+                -Constants.UI_HORIZONTAL_SPACING,
+                divSpacing,
+                -Constants.UI_HORIZONTAL_SPACING
+            )
+
+        row()
     }
 }
--- core/src/main/kotlin/ogz/tripeaks/screens/stage/StatisticsDialog.kt
+++ core/src/main/kotlin/ogz/tripeaks/screens/stage/StatisticsDialog.kt
@@ -3,22 +3,22 @@ package ogz.tripeaks.screens.stage
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.g2d.TextureAtlas
 import com.badlogic.gdx.math.MathUtils
-import com.badlogic.gdx.scenes.scene2d.Actor
-import com.badlogic.gdx.scenes.scene2d.ui.Cell
 import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup
 import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane
 import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable
 import com.badlogic.gdx.utils.Align
 import com.badlogic.gdx.utils.I18NBundle
 import com.ray3k.stripe.PopTable
+import ogz.tripeaks.Constants
 import ogz.tripeaks.assets.BundleAssets
 import ogz.tripeaks.assets.TextureAtlasAssets
 import ogz.tripeaks.assets.UiSkin
 import ogz.tripeaks.assets.get
+import ogz.tripeaks.models.LayoutStatistics
 import ogz.tripeaks.models.PlayerStatistics
-import ogz.tripeaks.Constants
 import ogz.tripeaks.ui.LabelButton
 
 class StatisticsDialog(
@@ -58,21 +58,11 @@ class StatisticsDialog(
         row()
 
         val table = Table(skin).apply {
-            defaults().align(Align.left).padBottom(skin.extraLineSpacing)
-
             padRight(Constants.UI_HORIZONTAL_SPACING)
+            defaults().align(Align.left).padBottom(Constants.UI_VERTICAL_SPACING).expandX().fillX()
 
             // General stats
-            addStat(
-                this,
-                bundle,
-                line,
-                bundle["statAll"],
-                games,
-                wins,
-                longestChain,
-                true
-            )
+            add(statPanel(bundle, uiAssets, LayoutStatistics("statAll", games, wins, longestChain)))
 
             row()
 
@@ -82,17 +72,8 @@ class StatisticsDialog(
                 .sortedBy { -it.played }
                 .withIndex()
                 .forEach { (index, layout) ->
-                    addStat(
-                        this,
-                        bundle,
-                        line,
-                        bundle[layout.tag],
-                        layout.played,
-                        layout.won,
-                        layout.longestChain,
-                        index != lastIndex
-                    )
-
+                    val cell = add(statPanel(bundle, uiAssets, layout))
+                    if (index == lastIndex) cell.padBottom(0f)
                     row()
                 }
         }
@@ -120,63 +101,56 @@ class StatisticsDialog(
         isHideOnUnfocus = false
     }
 
-    private fun addStat(
-        table: Table,
+    private fun statPanel(
         bundle: I18NBundle,
-        line: TextureAtlas.AtlasRegion,
-        title: String,
-        played: Int,
-        won: Int,
-        longestChain: Int,
-        padLast: Boolean = false
-    ) {
+        uiAssets: TextureAtlas,
+        stats: LayoutStatistics
+    ): Table = Table(skin).apply {
         val uiSkin = skin as UiSkin
+        val line = uiAssets.findRegion("${uiSkin.resourcePrefix}_line")
         val divSpacing = MathUtils.floor(uiSkin.extraLineSpacing / 2f).toFloat().coerceAtLeast(2f)
-        val lineSpacing = uiSkin.extraLineSpacing.coerceAtLeast(2f)
-        val columnSpacing = Constants.UI_HORIZONTAL_SPACING * 4f
-        val indent = Constants.UI_HORIZONTAL_SPACING * 2f
+        val bg = NinePatchDrawable(uiAssets.createPatch("${uiSkin.resourcePrefix}_panel"))
 
-        table.apply {
-            add(Label(title, skin, UiSkin.TITLE_LABEL_STYLE))
-                .colspan(2)
-                .align(Align.left)
-                .padBottom(uiSkin.extraLineSpacing)
+        pad(
+            Constants.UI_VERTICAL_SPACING,
+            Constants.UI_HORIZONTAL_SPACING,
+            Constants.UI_VERTICAL_SPACING,
+            Constants.UI_HORIZONTAL_SPACING,
+        )
+        setBackground(bg)
+        defaults().padBottom(uiSkin.extraLineSpacing.coerceAtLeast(2f)).left()
 
-            row()
+        add(Label(bundle[stats.tag], skin, UiSkin.TITLE_LABEL_STYLE))
+            .colspan(2)
+            .align(Align.left)
 
-            add(Image(line))
-                .colspan(2)
-                .expandX()
-                .fillX()
-                .pad(divSpacing, 0f, divSpacing, 0f)
+        row()
 
-            row()
+        add(Image(line))
+            .colspan(2)
+            .expandX()
+            .fillX()
+            .pad(
+                divSpacing,
+                -Constants.UI_HORIZONTAL_SPACING,
+                divSpacing,
+                -Constants.UI_HORIZONTAL_SPACING
+            )
 
-            add(Label(bundle["statGames"], skin))
-                .padRight(columnSpacing)
-                .padLeft(indent)
-                .padBottom(lineSpacing)
+        row()
 
-            add(Label(played.toString(), skin)).align(Align.right).padBottom(lineSpacing)
+        add(Label(bundle["statGames"], skin))
+        add(Label(stats.played.toString(), skin)).right()
 
-            row()
+        row()
 
-            add(Label(bundle["statWins"], skin))
-                .padRight(columnSpacing)
-                .padLeft(indent)
-                .padBottom(lineSpacing)
-            add(Label(won.toString(), skin)).align(Align.right).padBottom(lineSpacing)
+        add(Label(bundle["statWins"], skin))
+        add(Label(stats.won.toString(), skin)).right()
 
-            row()
+        row()
 
-            add(Label(bundle["statLongestChain"], skin))
-                .padRight(columnSpacing)
-                .padLeft(indent)
-                .padBottom(if (padLast) Constants.UI_VERTICAL_SPACING else 0f)
-            add(Label(longestChain.toString(), skin))
-                .align(Align.right)
-                .padBottom(if (padLast) Constants.UI_VERTICAL_SPACING else 0f)
-        }
+        add(Label(bundle["statLongestChain"], skin))
+        add(Label(stats.longestChain.toString(), skin)).right()
     }
 
 }
