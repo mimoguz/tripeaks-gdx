1e11ffb952b092f75ff345a4d77fead5f456db4c
Oguz Tas <mimoguz@outlook.com>
2024-08-17 13:19:20 +0300

    Change back to full screen

--- android/src/main/kotlin/ogz/tripeaks/android/AndroidLauncher.kt
+++ android/src/main/kotlin/ogz/tripeaks/android/AndroidLauncher.kt
@@ -1,7 +1,6 @@
 package ogz.tripeaks.android
 
 import android.os.Bundle
-import android.widget.RelativeLayout
 
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
@@ -11,14 +10,14 @@ import ogz.tripeaks.Main
 class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val layout = RelativeLayout(this)
-        layout.addView(initializeForView(Main(), AndroidApplicationConfiguration().apply {
-            useImmersiveMode = false
-        }))
-        setContentView(layout)
-        // initialize(Main(), AndroidApplicationConfiguration().apply {
+        // val layout = RelativeLayout(this)
+        // layout.addView(initializeForView(Main(), AndroidApplicationConfiguration().apply {
+        //     useImmersiveMode = false
+        // }))
+        // setContentView(layout)
+        initialize(Main(), AndroidApplicationConfiguration().apply {
             // Configure your application here.
-            // useImmersiveMode = false // Recommended, but not required.
-        // })
+            useImmersiveMode = true // Recommended, but not required.
+        })
     }
 }
