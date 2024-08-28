fb753b5609e27677587c2762aecac1fab7efe8d3
Oguz Tas <mimoguz@outlook.com>
2024-08-28 11:47:26 +0300

    Edit black theme colors

--- core/src/main/kotlin/ogz/tripeaks/Constants.kt
+++ core/src/main/kotlin/ogz/tripeaks/Constants.kt
@@ -39,10 +39,10 @@ object Constants {
 
     val DARK_UI_TEXT = rgb(249, 219, 169)
     val LIGHT_UI_TEXT = rgb(76, 56, 77)
-    val BLACK_UI_TEXT = rgb(141, 237, 116)
+    val BLACK_UI_TEXT = rgb(249, 219, 169)
     val DARK_UI_EMPHASIS = rgb(224, 122, 95)
     val LIGHT_UI_EMPHASIS = rgb(184, 55, 68)
-    val BLACK_UI_EMPHASIS = rgb(254, 91, 69)
+    val BLACK_UI_EMPHASIS = rgb(224, 122, 95)
     val DARK_BG: Color = Color.valueOf("232433ff")
     val LIGHT_BG: Color = Color.valueOf("63a347ff")
     val BLACK_BG: Color = Color.BLACK
