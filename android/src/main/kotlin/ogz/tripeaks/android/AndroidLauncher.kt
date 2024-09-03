a2bdd652a1ee22b21877ed9412611c47b45f42ed
Oguz Tas <mimoguz@outlook.com>
2024-09-03 18:34:23 +0300

    Remove commented-out code

--- android/src/main/kotlin/ogz/tripeaks/android/AndroidLauncher.kt
+++ android/src/main/kotlin/ogz/tripeaks/android/AndroidLauncher.kt
@@ -11,25 +11,11 @@ class AndroidLauncher : AndroidApplication() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val layout = GridLayout(this).also {
-//            it.columnCount = 1
-//            it.rowCount = 1
-//        }
-//        val view = initializeForView(Main(), AndroidApplicationConfiguration().apply {
-//            useImmersiveMode = false
-//        }).also {
-//            it.layoutParams = GridLayout.LayoutParams().also { it.setGravity(10) }
-//        }
-//        layout.addView(view)
-//        setContentView(layout)
-
         val modeFlags = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
         val darkMode = modeFlags == Configuration.UI_MODE_NIGHT_YES
         initialize(Main(darkMode), AndroidApplicationConfiguration().apply {
             // Configure your application here.
             useImmersiveMode = true // Recommended, but not required.
         })
-
-
     }
 }
