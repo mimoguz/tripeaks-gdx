38325af4f32ec7b1c43b54fd32c85d1327d9618c
Oguz Tas <mimoguz@outlook.com>
2024-09-13 13:46:23 +0300

    Set window size

--- lwjgl3/src/main/kotlin/ogz/tripeaks/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/ogz/tripeaks/lwjgl3/Lwjgl3Launcher.kt
@@ -4,6 +4,7 @@ package ogz.tripeaks.lwjgl3
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import ogz.tripeaks.Constants
 import ogz.tripeaks.Main
 
 /** Launches the desktop (LWJGL3) application. */
@@ -12,7 +13,7 @@ fun main() {
     if (StartupHelper.startNewJvmIfRequired()) return
     Lwjgl3Application(Main(false), Lwjgl3ApplicationConfiguration().apply {
         setTitle("TriPeaks")
-        setWindowedMode(640, 480)
+        setWindowedMode(Constants.MIN_WORLD_WIDTH.toInt() * 3, Constants.WORLD_HEIGHT.toInt() * 3)
         setWindowIcon(*(arrayOf(256, 128, 64, 32, 16).map { "tripeaks-gdx$it.png" }.toTypedArray()))
     })
 }
